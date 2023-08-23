b02f5e76cbabcf8fdc338cb896673b5d59b0eb85
ndzy01 <2326962137@qq.com>
2023-08-23 20:35:48 +0800

    chore: 优化

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -2,6 +2,43 @@ App<IAppOption>({
   globalData: {},
   onLaunch() {
     wx.setStorageSync('token', '');
+    wx.setStorageSync(
+      'local',
+      JSON.stringify([
+        {
+          createdAt: '2023-08-22T22:04:24.470Z',
+          detail: '1. 每天至少运动一个小时\n2. 尽量在十点钟之前完成\n',
+          detailHtml: '<ol>\n<li>每天至少运动一个小时</li>\n<li>尽量在十点钟之前完成</li>\n</ol>\n',
+          id: 'ecfe7e3e-ebee-42ab-a08d-7245463e5353',
+          isDel: 0,
+          link: '',
+          name: '运动',
+          deadline: '2023-08-31',
+          updatedAt: '2023-08-23T08:11:09.000Z',
+          version: 3,
+          tagName: '生活',
+          tagId: '37c64bf2-a215-4fa6-8f9a-792a8586fcb5',
+          userName: '游客',
+          isDemo: true,
+        },
+        {
+          createdAt: '2023-08-22T19:17:52.858Z',
+          detail: '## 十点前上床睡觉',
+          detailHtml: '<h2>十点前上床睡觉</h2>',
+          id: '6df6b214-53ef-4635-9aa4-a788ae52feaa',
+          isDel: 1,
+          link: '',
+          name: '早睡',
+          deadline: '2023-08-23',
+          updatedAt: '2023-08-22T22:02:35.000Z',
+          version: 12,
+          tagName: '生活',
+          tagId: '37c64bf2-a215-4fa6-8f9a-792a8586fcb5',
+          userName: '游客',
+          isDemo: true,
+        },
+      ]),
+    );
 
     // 登录
     wx.login({
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,42 +1,7 @@
 import request from '../../http';
 
-const demoData = [
-  {
-    createdAt: '2023-08-22T22:04:24.470Z',
-    detail: '1. 每天至少运动一个小时\n2. 尽量在十点钟之前完成\n',
-    detailHtml: '<ol>\n<li>每天至少运动一个小时</li>\n<li>尽量在十点钟之前完成</li>\n</ol>\n',
-    id: 'ecfe7e3e-ebee-42ab-a08d-7245463e5353',
-    isDel: 0,
-    link: '',
-    name: '运动',
-    deadline: '2023-08-31',
-    updatedAt: '2023-08-23T08:11:09.000Z',
-    version: 3,
-    tagName: '生活',
-    tagId: '37c64bf2-a215-4fa6-8f9a-792a8586fcb5',
-    userName: '游客',
-    isDemo: true,
-  },
-  {
-    createdAt: '2023-08-22T19:17:52.858Z',
-    detail: '## 十点前上床睡觉',
-    detailHtml: '<h2>十点前上床睡觉</h2>',
-    id: '6df6b214-53ef-4635-9aa4-a788ae52feaa',
-    isDel: 1,
-    link: '',
-    name: '早睡',
-    deadline: '2023-08-23',
-    updatedAt: '2023-08-22T22:02:35.000Z',
-    version: 12,
-    tagName: '生活',
-    tagId: '37c64bf2-a215-4fa6-8f9a-792a8586fcb5',
-    userName: '游客',
-    isDemo: true,
-  },
-];
-
 Page<
-  { todos: { id: string; isDel: number }[]; isDemo: boolean },
+  { todos: { id: string; isDel: number }[]; isLocal: boolean },
   {
     goEditPage: (e: any) => void;
     getAll: () => void;
@@ -47,7 +12,7 @@ Page<
 >({
   data: {
     todos: [],
-    isDemo: false,
+    isLocal: false,
   },
   onPullDownRefresh() {
     this.onLoad();
@@ -76,28 +41,29 @@ Page<
       .then((res: any) => {
         this.setData({
           todos: res.data,
-          isDemo: false,
+          isLocal: false,
         });
       })
       .catch(() => {
         this.setData({
-          isDemo: true,
-          todos: JSON.parse(JSON.stringify(demoData)),
+          isLocal: true,
+          todos: JSON.parse(wx.getStorageSync('local')),
         });
       });
   },
   finishTodo(e) {
     const { id } = e.target.dataset;
-    if (this.data.isDemo) {
-      this.setData({
-        isDemo: true,
-        todos: this.data.todos.map((item) => {
-          if (item.id === id) {
-            item.isDel = 1;
-          }
-          return item;
-        }),
+    if (this.data.isLocal) {
+      const todos = this.data.todos.map((item) => {
+        if (item.id === id) {
+          item.isDel = 1;
+        }
+        return item;
       });
+      this.setData({
+        todos,
+      });
+      wx.setStorageSync('local', JSON.stringify(todos));
     } else {
       request({ url: `/todos/${id}`, method: 'PATCH', data: { isDel: true } }).then(() => {
         this.getAll();
@@ -106,11 +72,12 @@ Page<
   },
   delTodo(e) {
     const { id } = e.target.dataset;
-    if (this.data.isDemo) {
+    if (this.data.isLocal) {
+      const todos = this.data.todos.filter((item) => item.id !== id);
       this.setData({
-        isDemo: true,
-        todos: this.data.todos.filter((item) => item.id !== id),
+        todos,
       });
+      wx.setStorageSync('local', JSON.stringify(todos));
     } else {
       request({ url: `/todos/${id}`, method: 'DELETE' }).then(() => {
         this.getAll();
@@ -119,16 +86,17 @@ Page<
   },
   recoverTodo(e) {
     const { id } = e.target.dataset;
-    if (this.data.isDemo) {
-      this.setData({
-        isDemo: true,
-        todos: this.data.todos.map((item) => {
-          if (item.id === id) {
-            item.isDel = 0;
-          }
-          return item;
-        }),
+    if (this.data.isLocal) {
+      const todos = this.data.todos.map((item) => {
+        if (item.id === id) {
+          item.isDel = 0;
+        }
+        return item;
       });
+      this.setData({
+        todos,
+      });
+      wx.setStorageSync('local', JSON.stringify(todos));
     } else {
       request({ url: `/todos/${id}`, method: 'PATCH', data: { isDel: false } }).then(() => {
         this.getAll();
