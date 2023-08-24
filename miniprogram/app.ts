f665fea3cd0bd80aa34e3c565007da4a10175e15
ndzy01 <2326962137@qq.com>
2023-08-24 12:17:21 +0800

    chore: 版本更新提示

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -39,10 +39,33 @@ App<IAppOption>({
         },
       ]),
     );
-
     // 登录
     wx.login({
       success: (_res) => {},
     });
+    const updateManager = wx.getUpdateManager();
+    if (updateManager) {
+      updateManager.onCheckForUpdate(function (res) {
+        if (res.hasUpdate) {
+          updateManager.onUpdateReady(function () {
+            wx.showModal({
+              title: '更新提示',
+              content: '新版本已经准备好，是否重启应用？',
+              success: function (res) {
+                if (res.confirm) {
+                  updateManager.applyUpdate();
+                }
+              },
+            });
+          });
+          updateManager.onUpdateFailed(function () {
+            wx.showModal({
+              title: '更新提示',
+              content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。',
+            });
+          });
+        }
+      });
+    }
   },
 });
