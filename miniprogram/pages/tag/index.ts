1d3bff21965eb72ed535ae1e902fc3bf5ab1f9b4
张一 <zy01066705@alibaba-inc.com>
2024-02-18 14:06:02 +0800

    chore: 1

--- miniprogram/pages/tag/index.ts
+++ miniprogram/pages/tag/index.ts
@@ -1,7 +1,7 @@
 import { wxCloudRequest } from '../../http';
 
-Page<{ tags: any[] }, { [k: string]: any }>({
-  data: { tags: [] },
+Page<{ tags: any[]; name: string }, { [k: string]: any }>({
+  data: { tags: [], name: '' },
   onPullDownRefresh() {
     this.onLoad();
   },
@@ -15,6 +15,21 @@ Page<{ tags: any[] }, { [k: string]: any }>({
   init() {
     wxCloudRequest({ url: '/tags', method: 'GET' }).then((res: any) => {
       this.setData({ tags: res.data });
+    });
+  },
+  bindNameChange(e: any) {
+    this.setData({ name: e.detail });
+  },
+  bindAdd() {
+    wxCloudRequest({ url: '/tags', method: 'POST', data: { name: this.data.name } }).then(() => {
+      this.setData({ name: '' });
+      this.init();
+    });
+  },
+  bindDel(e: any) {
+    const { id } = e.target.dataset;
+    wxCloudRequest({ url: `/tags/${id}`, method: 'DELETE' }).then(() => {
+      this.init();
     });
   },
 });
